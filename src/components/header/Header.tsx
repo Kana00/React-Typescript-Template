55457bac7dda2b6093277a849867e3ff3d54dfd5
Kana00 <Illya.delsarte@gmail.com>
2020-03-24 20:46:28 +0100

    Improuve style architecture

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,18 +6,28 @@ export default class Header extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        <h2>{i18next.t('Title')}</h2>
-        <p>{i18next.t('description Application')}</p>
-        <ul>
-          <li>Webpack 4</li>
-          <li>TypeScript 3</li>
-          <li>React 16</li>
-          <li>Sass styling</li>
-          <li>React Router</li>
-          <li>Redux</li>
-          <li>i18next</li>
-          <li>Debugger with VS Code</li>
-        </ul>
+        <div id="header-bar">
+          <div id="header-content">
+            Website Title
+          </div>
+        </div>
+
+        <div className='content'>
+          <h1 id="title-site">{i18next.t('Title')}</h1>
+          <p>{i18next.t('description Application')}</p>
+
+          <ul>
+            <li>Webpack 4</li>
+            <li>TypeScript 3</li>
+            <li>React 16</li>
+            <li>Sass styling</li>
+            <li>React Router</li>
+            <li>Redux</li>
+            <li>i18next</li>
+            <li>Unit testing with Jest</li>
+            <li>Debugger with VS Code</li>
+          </ul>
+        </div>
         <hr className='hr' />
       </div>
     );
